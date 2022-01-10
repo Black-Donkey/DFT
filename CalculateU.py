4f226c61c439f754a4d17d47d306a5eb97cb4d9a
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-01-10 02:12:41 -0500

    update function get orbital

--- CalculateU.py
+++ CalculateU.py
@@ -5,49 +5,44 @@ import linecache
 # from prettytable import PrettyTable
 
 
-def get_total_charge(str_path, str_keyword):
-    num_lines = sum(1 for line in open(str_path))
-    line_num = 0
-    msg = "There is no result"
+def get_total_charge_first_line(str_path, str_keyword):
+    int_total_lines = sum(1 for line in open(str_path))
+    int_line_idx = 0
+    msg = "[Error]: There is no result"
     with open(str_path, 'r') as file:
         for line in reversed(file.readlines()):
-            line_num = line_num + 1
+            int_line_idx = int_line_idx + 1
             if str_keyword in line.strip():
-                msg = "'%s' string in line %d" % (str_keyword, num_lines - line_num + 1)
+                msg = "'%s' string in line %d" % (str_keyword, int_total_lines - int_line_idx + 1)
                 break
     print(msg)
-    return num_lines - line_num + 1
+    return int_total_lines - int_line_idx + 1 + 4
 
 
-def get_d_orbital_charge(str_input):
-    num = [float(s) for s in str_input.split(" ")]
-    return num
+def get_orbital_charge(str_input, str_orbital):
+    if str_orbital == 's':
+        return float(str_input.split()[1])
+    if str_orbital == 'p':
+        return float(str_input.split()[2])
+    if str_orbital == 'd':
+        return float(str_input.split()[3])
+    if str_orbital == 'f':
+        return float(str_input.split()[4])
 
 
 def main():
     # Variables
-    path = "S:\\projects\\05_LLTO_2N_Ov_ISIF_3_U\\LLTO-2N-5-OV-groundstate-MAG\\OUTCAR"
-    f = open(path)
+    str_path = "S:\\projects\\05_LLTO_2N_Ov_ISIF_3_U\\LLTO-2N-5-OV-groundstate-MAG\\OUTCAR"
+    int_La_idx = 108
+    int_Ti_idx = 128
+    int_Li_idx = 164
+    # Read OUTCAR
+    f = open(str_path)
     f.close()
-    line_num = get_total_charge(path, "total charge")
-    theline = linecache.getline(path, line_num + 4)
-    print(theline)
-    # for lines in reversed(lines):
-    #     if 'total charge' in lines:
-    #         print(lines)
-    #         break
+    int_line_idx = get_total_charge_first_line(str_path, "total charge")
 
-    # groundstate_path = Structure.from_file(path + "LLTO-2N-5-OV-1-ISIF0-CONTCAR")
-
-    # Make Table
-    # pt = PrettyTable()
-    # pt.field_names = ["Index", "Formula", "N+ Fraction", "Volume", "Lattice Angles", "Lattice Number"]
-    #
-    # pt.add_row(
-    #     ["LLTO-2N-5-OV-1-ISIF0", s2.formula, s2.composition.get_atomic_fraction("N"), s2.volume, s2.lattice.angles,
-    #      s2.lattice.abc])
-
-    # print(pt)
+    str_line = linecache.getline(str_path, int_line_idx + int_La_idx - 1)
+    print(get_orbital_charge(str_line, 'd'))
 
 
 if __name__ == '__main__':
