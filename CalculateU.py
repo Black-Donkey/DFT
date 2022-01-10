1156e4b5fcca7c9a6dc7938f1dc08abc51c9b56a
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-01-10 01:42:44 -0500

    new function get orbital

--- CalculateU.py
+++ CalculateU.py
@@ -1,31 +1,36 @@
 import linecache
+
+
 # from pymatgen.core import Structure
 # from prettytable import PrettyTable
 
 
-def filetest(path, strval):
-    num_lines = sum(1 for line in open(path))
+def get_total_charge(str_path, str_keyword):
+    num_lines = sum(1 for line in open(str_path))
     line_num = 0
     msg = "There is no result"
-    with open(path, 'r') as file:
+    with open(str_path, 'r') as file:
         for line in reversed(file.readlines()):
             line_num = line_num + 1
-            if strval in line.strip():
-                msg = "'%s' string in line %d" % (strval, num_lines - line_num + 1)
+            if str_keyword in line.strip():
+                msg = "'%s' string in line %d" % (str_keyword, num_lines - line_num + 1)
                 break
     print(msg)
     return num_lines - line_num + 1
+
+
+def get_d_orbital_charge(str_input):
+    num = [float(s) for s in str_input.split(" ")]
+    return num
 
 
 def main():
     # Variables
     path = "S:\\projects\\05_LLTO_2N_Ov_ISIF_3_U\\LLTO-2N-5-OV-groundstate-MAG\\OUTCAR"
     f = open(path)
-    lines = f.readlines()
     f.close()
-    # print(lines.index("total charge"))
-    line_num = filetest(path, "total charge")
-    theline = linecache.getline(path, line_num+4)
+    line_num = get_total_charge(path, "total charge")
+    theline = linecache.getline(path, line_num + 4)
     print(theline)
     # for lines in reversed(lines):
     #     if 'total charge' in lines:
