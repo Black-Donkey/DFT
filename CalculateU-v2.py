da2db8973abd836c82244e5e26ce2f3c0ade97b4
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-01-10 21:38:41 -0500

    update v2

--- CalculateU-v2.py
+++ CalculateU-v2.py
@@ -8,8 +8,7 @@ import pandas as pd
 # function getting the index of the first line for total charge
 def get_total_charge_first_line(str_path, str_keyword):
     str_path_outcar = str_path + "OUTCAR"
-    str_path_poscar = str_path + "POSCAR"
-    int_total_lines = sum(1 for line in open(str_path_outcar))
+    int_total_lines = sum(1 for _ in open(str_path_outcar))
     int_line_idx = 0
     msg = "[Error]: There is no result"
     with open(str_path_outcar, 'r') as file:
@@ -36,19 +35,30 @@ def get_orbital_charge(str_input, str_orbital):
 
 
 # function saving the total charge data into csv file
-def save_total_charge(str_path):
+def save_total_charge(str_path, str_csv_file_name):
     str_path_outcar = str_path + "OUTCAR"
     str_path_poscar = str_path + "POSCAR"
+    # return dictionary
+    lst_element = []
+    lst_element_num = []
+    int_element_num = 0
+    int_atom_types = len(linecache.getline(str_path_poscar, 1).split())
+    for idx in range(0, int_atom_types):
+        lst_element.append(linecache.getline(str_path_poscar, 1).split()[idx])
+        int_element_num = int_element_num + int(linecache.getline(str_path_poscar, 6).split()[idx])
+        lst_element_num.append(int_element_num)
+    print(lst_element)
+    print(lst_element_num)
     int_total_atoms = sum(map(int, linecache.getline(str_path_poscar, 6).split()))
+    dic_element = dict(zip(lst_element, lst_element_num))
+    # save csv file
     int_first_line = get_total_charge_first_line(str_path, "total charge")
-
     lst_s_charge = []
     lst_p_charge = []
     lst_d_charge = []
     lst_f_charge = []
-
-    for idx in range(1, int_total_atoms):
-        str_line = linecache.getline(str_path_outcar, int_first_line + idx - 1)
+    for idx in range(0, int_total_atoms):
+        str_line = linecache.getline(str_path_outcar, int_first_line + idx)
         flt_s_charge = (get_orbital_charge(str_line, 's'))
         flt_p_charge = (get_orbital_charge(str_line, 'p'))
         flt_d_charge = (get_orbital_charge(str_line, 'd'))
@@ -59,25 +69,34 @@ def save_total_charge(str_path):
         lst_f_charge.append(flt_f_charge)
     dic_data = {'s': lst_s_charge, 'p': lst_p_charge, 'd': lst_d_charge, 'f': lst_f_charge}
     output = pd.DataFrame(dic_data)
-    output.to_csv("data.csv", index=False, encoding='utf8')
+    output.to_csv(str_csv_file_name, index=False, encoding='utf8')
+    return dic_element
 
 
 def main():
     # Variables
     str_path_groundstate = "S:\\projects\\05_LLTO_2N_Ov_ISIF_3_U\\LLTO-2N-5-OV-groundstate-MAG\\"
-    #
-    # int_La_idx = 108
-    # int_Ti_idx = 128
-    # int_Li_idx = 164
-    # Read OUTCAR
-    # f = open(str_path_groundstate)
-    # f.close()
-    # int_first_line = get_total_charge_first_line(str_path_groundstate, "total charge")
-    # for idx in range(int_La_idx, int_Ti_idx):
-    #     str_line = linecache.getline(str_path_groundstate, int_first_line + idx - 1)
-    #     print(get_orbital_charge(str_line, 'd'))
-    #     print(get_orbital_charge(str_line, 'f'))
-    save_total_charge(str_path_groundstate)
+    str_path_nsf = "S:\\projects\\05_LLTO_2N_Ov_ISIF_3_U\\LLTO-2N-5-OV-NSF\\"
+    str_path_sf = "S:\\projects\\05_LLTO_2N_Ov_ISIF_3_U\\LLTO-2N-5-OV-SF\\"
+    # Save csv files
+    dic_element = save_total_charge(str_path_groundstate, "groundstate_total_charge.csv")
+    save_total_charge(str_path_nsf, "NSF_total_charge.csv")
+    save_total_charge(str_path_sf, "SF_total_charge.csv")
+    # Load csv files
+    flt_charge_groundstate = pd.read_csv('groundstate_total_charge.csv')
+    flt_charge_nsf = pd.read_csv('NSF_total_charge.csv')
+    flt_charge_sf = pd.read_csv('SF_total_charge.csv')
+    # Calculate La
+    for idx in range(dic_element['N'], dic_element['La']):
+        # d orbital
+        flt_delta_nsf = flt_charge_nsf.iloc[idx]["d"] - flt_charge_groundstate.iloc[idx]["d"]
+        flt_delta_sf = flt_charge_sf.iloc[idx]["d"] - flt_charge_groundstate.iloc[idx]["d"]
+        u_la_d = 1/flt_delta_sf - 1/flt_delta_nsf
+        # f orbital
+        flt_delta_nsf = flt_charge_nsf.iloc[idx]["f"] - flt_charge_groundstate.iloc[idx]["f"]
+        flt_delta_sf = flt_charge_sf.iloc[idx]["f"] - flt_charge_groundstate.iloc[idx]["f"]
+        u_la_f = 1 / flt_delta_sf - 1 / flt_delta_nsf
+    # Calculate Ti
 
 
 if __name__ == '__main__':
