7db8cd13d0a171e4db0396f46ab60213c65da0af
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-01-24 14:46:39 -0500

    update v2

--- Energy_Monitor_Simple.py
+++ Energy_Monitor_Simple.py
@@ -3,6 +3,7 @@ import linecache
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationToolbar2Tk)
+import paramiko
 
 
 # function getting the line index of the total charge string existing last time
@@ -51,6 +52,14 @@ def generate_canvas(self):
     self.toolbar.place(x=20, y=475)
 
 
+def check_this(self):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect('baigroup.duckdns.org', username='jiachengwang', password='wjc19950910')
+    str_path = self.entry01.get()
+    client.exec_command('cd ' + str_path + ';shotgun check-this -s')
+
+
 def main():
     root = Tk()
     root.title("ENERGY MONITOR")
@@ -66,9 +75,14 @@ def main():
     root.entry01.place(x=20, y=60, width=700, height=30)
     v1.set("S:\\projects\\04_LLTO_2N_Ov_ISIF_3\\LLTO-2N-5-OV-ISIF3-2\\STEP2\\")
 
-    # Button
+    # Button Plot
     root.btn01 = Button(root, font=("Arial", 11), text="Plot", command=lambda: generate_canvas(root))
     root.btn01.place(x=820, y=60, width=65, height=30)
+
+    # Button Check-this
+    root.btn02 = Button(root, font=("Arial", 11), text="Check-this", command=lambda: check_this(root))
+    root.btn02.place(x=730, y=60, width=85, height=30)
+
     root.mainloop()
 
 
