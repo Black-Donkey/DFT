9dc28ea4372d2084c508f75ad400a59d95c7a325
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-01-26 12:19:03 -0500

    update v2

--- DOSplot.py
+++ DOSplot.py
@@ -10,9 +10,14 @@ def main():
 
     pt = PrettyTable()
     pt.field_names = ["Index", "Formula", "N+ Fraction", "Volume", "Lattice Angles", "Lattice Number"]
-    pt.add_row(["LLTO-2N-5-OV-1", s1.formula, s1.composition.get_atomic_fraction("N"), s1.volume, s1.lattice.angles, s1.lattice.abc])
-    pt.add_row(["LLTO-2N-5-OV-1-ISIF0", s2.formula, s2.composition.get_atomic_fraction("N"), s2.volume, s2.lattice.angles, s2.lattice.abc])
-    pt.add_row(["LLTO-2N-5-OV-1-ISIF3", s3.formula, s3.composition.get_atomic_fraction("N"), s3.volume, s3.lattice.angles, s3.lattice.abc])
+    pt.add_row(["LLTO-2N-5-OV-1", s1.formula, s1.composition.get_atomic_fraction("N"), s1.volume, s1.lattice.angles,
+                s1.lattice.abc])
+    pt.add_row(
+        ["LLTO-2N-5-OV-1-ISIF0", s2.formula, s2.composition.get_atomic_fraction("N"), s2.volume, s2.lattice.angles,
+         s2.lattice.abc])
+    pt.add_row(
+        ["LLTO-2N-5-OV-1-ISIF3", s3.formula, s3.composition.get_atomic_fraction("N"), s3.volume, s3.lattice.angles,
+         s3.lattice.abc])
     print(pt)
 
 
