3c4209b60320fd152ca61808a44595a8b7f037f6
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-02-01 00:38:14 -0500

    create energy plot

--- DOSplot.py
+++ DOSplot.py
@@ -9,14 +9,38 @@ def main():
 
     dos_vasprun = Vasprun(path + "vasprun.xml")
     dos_data = dos_vasprun.complete_dos
-    bs_vasprun = Vasprun(path + "vasprun.xml", parse_projected_eigen=True)
-    bs_data = bs_vasprun.get_band_structure()
+    # bs_vasprun = Vasprun(path + "vasprun.xml", parse_projected_eigen=True)
+    # bs_data = bs_vasprun.get_band_structure()
 
     # Total dos calculated at the end of run.
     tdos = dos_vasprun.tdos
     plot1 = DosPlotter(stack=False, sigma=0.5)
     plot1.add_dos("Total DOS", tdos)
     plot1.show()
+
+    run = BSVasprun(path + "vasprun.xml", parse_projected_eigen=True)
+    bs = run.get_band_structure("KPOINTS")
+    print("number of bands", bs.nb_bands)
+    print("number of kpoints", len(bs.kpoints))
+    print(bs.is_metal())
+    print(bs.is_spin_polarized)
+    # bsplot = BSPlotter(bs)
+    # # get the plot
+    # bsplot.get_plot(ylim=(-20, 10), zero_to_efermi=True)
+    # print(bs.efermi)
+    #
+    # # add some features
+    # ax = plt.gca()
+    # ax.set_title("N-LLTO Band Structure", fontsize=20)
+    # xlim = ax.get_xlim()
+    # ax.hlines(0, xlim[0], xlim[1], linestyles="dashed", color="black")
+    #
+    # # add legend
+    # ax.plot((), (), "b-", label="spin up")
+    # ax.plot((), (), "r--", label="spin down")
+    # ax.legend(fontsize=16, loc="upper left")
+
+    # ValueError: BSPlotter only works with BandStructureSymmLine objects. A BandStructure object (on a uniform grid for instance and not along symmetry lines won't work)
 
     # v = BSVasprun(path + "vasprun.xml")
     # bs = v.get_band_structure(kpoints_filename="band/KPOINTS", line_mode=True)
