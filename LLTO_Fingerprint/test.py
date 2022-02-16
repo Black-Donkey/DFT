e91de7650d62a4262ba34542fbed0767e2eed224
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-02-16 15:35:04 -0500

    update v2

--- LLTO_Fingerprint/test.py
+++ LLTO_Fingerprint/test.py
@@ -1,12 +1,26 @@
-from pymatgen.core import Structure
+from pymatgen.core import Structure, Lattice
 from prettytable import PrettyTable
 import pandas as pd
-
-
-def main():
-    # path1 = "S:\\projects\\07_LLTO_ISIF_ISPIN_DIFF\\01_LLTO_ISIF0_ISPIN1\\STEP2\\"
-    s1 = Structure.from_file("U2_N0_OV0_1.cif")
-
+import matplotlib as plt
 
 if __name__ == '__main__':
-    main()
+    coordinates = []
+    species = []
+    neighbor = []
+    structure_from_cif = Structure.from_file("U2_N0_OV0_1.cif")
+    for s in structure_from_cif:
+        print(s)
+        coordinates.append(s.frac_coords.tolist())
+        species.append(s.specie.Z)
+        # neighbor.append(s.get_neighbors(r=2))
+    element = set(species)
+
+    neighbor = structure_from_cif.get_all_neighbors(r=3)
+    # ax = plt.subplot(111, projection='3d')  # 创建一个三维的绘图工程
+    #
+    # ax.scatter(x[:10], y[:10], z[:10], c='y')  # 绘制数据点
+    #
+    # ax.set_zlabel('Z')  # 坐标轴
+    # ax.set_ylabel('Y')
+    # ax.set_xlabel('X')
+    # plt.show()
