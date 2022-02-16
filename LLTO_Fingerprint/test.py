e05c8192a6893e2594c3706a9589db3fd28b58c1
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-02-16 11:29:34 -0500

    update v2

--- LLTO_Fingerprint/test.py
+++ LLTO_Fingerprint/test.py
@@ -1,1 +1,12 @@
+from pymatgen.core import Structure
+from prettytable import PrettyTable
+import pandas as pd
 
+
+def main():
+    # path1 = "S:\\projects\\07_LLTO_ISIF_ISPIN_DIFF\\01_LLTO_ISIF0_ISPIN1\\STEP2\\"
+    s1 = Structure.from_file("U2_N0_OV0_1.cif")
+
+
+if __name__ == '__main__':
+    main()
