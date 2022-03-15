3b93eff662e46a27a93ea305cda1d08fb596ea48
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-03-15 15:55:16 -0400

    update v2

--- Plot_Template/N2_OV0_La_Li_N_Energy_Plot.py
+++ Plot_Template/N2_OV0_La_Li_N_Energy_Plot.py
@@ -1,22 +1,27 @@
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from mpl_toolkits.mplot3d import Axes3D
 import pandas as pd
 
-
-path1 = "S:\\projects\\11_LLTO_U2_N1_OV0\\data_energy.csv"
+path1 = "S:\\projects\\13_LLTO_U2_N2_OV0\\data_energy.csv"
 csv_energy = pd.read_csv(path1, header=None)
 list_energy = sum(csv_energy.values.tolist(), [])
 
-La = [1, 2, 3, 4, 0, 1, 2, 3, 0, 1]
-Li = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2]
+La = [1, 2, 3, 4, 2, 0, 1, 2, 3, 2, 0, 1]
+Li = [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+N = [0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0]
+
 plot_symbol = ["o", "^", "s"]
 
+fig = plt.figure()
+ax = Axes3D(fig)
 for index in range(0, len(set(Li))):
     plot_La = [La[i] for i, x in enumerate(Li) if x == index]
+    plot_N = [N[i] for i, x in enumerate(Li) if x == index]
     plot_energy = [list_energy[i] for i, x in enumerate(Li) if x == index]
-    plt.scatter(plot_La, plot_energy, marker=plot_symbol[index], s=75)
-plt.legend(['# Li=0', '# Li=1', '# Li=2'])
-plt.xlabel('# La')
-plt.ylabel('Energy (eV)')
-plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+    ax.scatter(plot_La, plot_N, plot_energy, marker=plot_symbol[index], s=75)
+ax.legend(['# Li=0', '# Li=1', '# Li=2'])
+# ax.xlabel('# La')
+# ax.ylabel('Energy (eV)')
+# ax.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
 plt.show()
