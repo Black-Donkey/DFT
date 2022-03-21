2a835d5f07203f7824f23665cda6211f6da6807b
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-03-21 01:28:06 -0400

    update plot

--- Pymatgen/Conductivity/Calculate_Conductivity.py
+++ Pymatgen/Conductivity/Calculate_Conductivity.py
@@ -7,23 +7,23 @@ from pymatgen.analysis.diffusion_analyzer import DiffusionAnalyzer, \
 # from pymatgen.analysis.diffusion.aimd.pathway import ProbabilityDensityAnalysis
 # from pymatgen.analysis.diffusion.aimd.van_hove import VanHoveAnalysis
 
-temperatures = [500, 1000, 1500]
+temperatures = [300, 900]
 
 
 a = ["S:/projects/09_DFT_MD_test/LLTO_2U_1N_3/vasprun.xml"]
-b = ["S:/projects/09_DFT_MD_test/LLTO_2U_1N_3/vasprun.xml"]
-c = ["S:/projects/09_DFT_MD_test/LLTO_2U_1N_3/vasprun.xml"]
+b = ["S:/projects/25_LLTO_U2_MD/LLTO_U2_N0_OV0/vasprun.xml"]
 
-analyzer_500 = DiffusionAnalyzer.from_files(a, specie="Li", smoothed=False)
-analyzer_1000 = DiffusionAnalyzer.from_files(b, specie="Li", smoothed=False)
-analyzer_1500 = DiffusionAnalyzer.from_files(c, specie="Li", smoothed=False)
+analyzer_300 = DiffusionAnalyzer.from_files(a, specie="Li", smoothed=False)
+diffusivities = [analyzer_300.diffusivity, analyzer_300.diffusivity]
 
-diffusivities = [analyzer_500.diffusivity, analyzer_1000.diffusivity, analyzer_1500.diffusivity]
+# analyzer_900 = DiffusionAnalyzer.from_files(b, specie="Li", smoothed=False)
+
+# diffusivities = [analyzer_300.diffusivity, analyzer_900.diffusivity]
 
 fig = get_arrhenius_plot(temperatures, diffusivities)
 fig.savefig("Li_Arrhenius_Plot.png")
 fig.show()
 rts = get_extrapolated_conductivity(temperatures, diffusivities,
-                                    new_temp=300, structure=analyzer_500.structure,
+                                    new_temp=1200, structure=analyzer_300.structure,
                                     species="Li")
 print("The Li ionic conductivity for anti-spinel LLTO at 300 K is %.4f mS/cm" % rts)
