8e9390021cf6d2ac4355c61853d40da7cd852b57
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-03-28 22:38:18 -0400

    update v2

--- Average_Energy/Calculate_Average_Energy.py
+++ Average_Energy/Calculate_Average_Energy.py
@@ -12,8 +12,15 @@ U2_N2_OV0_path = "S:/projects/13_LLTO_U2_N2_OV0/data_energy.csv"
 # LLTO_U2_N1_OV0
 U2_N1_OV0_energy = sum(pd.read_csv(U2_N1_OV0_path, header=None).values.tolist(), [])
 U2_N1_OV0_count = [2, 38, 12, 18, 4, 4, 20, 2, 6, 2]
-Z = 0
-for idx in range(0, len(U2_N1_OV0_energy)):
-    Z += math.exp(-U2_N1_OV0_energy[idx] / (kb * T))
+
+
+# U2_N1_OV0_energy_0 = U2_N1_OV0_energy[:-1]
+# U2_N1_OV0_energy_1 = U2_N1_OV0_energy[1:]
+# U2_N1_OV0_energy_delta = np.array(U2_N1_OV0_energy_1) - np.array(U2_N1_OV0_energy_0)
+
+
+# Z = 0
+# for idx in range(0, len(U2_N1_OV0_energy_delta)):
+#     Z += math.exp(-U2_N1_OV0_energy_delta[idx] / (kb * T))
 
 # np.multiply(U2_N1_OV0_energy, U2_N1_OV0_count)
