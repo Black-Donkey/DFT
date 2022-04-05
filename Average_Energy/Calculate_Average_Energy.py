ca41540d7f8756adac732a678a4f42a18f121a47
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-04-05 10:19:03 -0400

    update v2

--- Average_Energy/Calculate_Average_Energy.py
+++ Average_Energy/Calculate_Average_Energy.py
@@ -10,6 +10,9 @@ U2_N1_OV0_path = "S:/projects/11_LLTO_U2_N1_OV0/data_energy.csv"
 U2_N2_OV0_path = "S:/projects/13_LLTO_U2_N2_OV0/data_energy.csv"
 
 # LLTO_U2_N1_OV0
+
+
+# LLTO_U2_N1_OV0
 U2_N1_OV0_energy = sum(pd.read_csv(U2_N1_OV0_path, header=None).values.tolist(), [])
 U2_N1_OV0_count = [2, 38, 12, 18, 4, 4, 20, 2, 6, 2]
 
@@ -18,9 +21,9 @@ U2_N1_OV0_energy_delta = [i-U2_N1_OV0_energy[U2_N1_OV0_energy_min_idx] for i in 
 
 Z = 0
 for idx in range(0, len(U2_N1_OV0_energy_delta)):
-    Z += math.exp(-U2_N1_OV0_energy_delta[idx] / (kb * T))
+    Z += U2_N1_OV0_count[idx] * np.exp(-U2_N1_OV0_energy_delta[idx] / (kb * T))
 sum_term = 0
 for idx in range(0, len(U2_N1_OV0_energy_delta)):
     sum_term += U2_N1_OV0_energy[idx] * U2_N1_OV0_count[idx] * np.exp(-U2_N1_OV0_energy_delta[idx] / (kb * T))
-average_energy = sum_term / Z
-print(average_energy)
+U2_N1_OV0_average_energy = sum_term / Z
+print(U2_N1_OV0_average_energy)
