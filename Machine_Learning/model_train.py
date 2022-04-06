12d04bbdcc76c9afe70c41e728b96f64b244b124
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-04-06 17:40:43 -0400

    update v2

--- Machine_Learning/model_train.py
+++ Machine_Learning/model_train.py
@@ -1,14 +1,75 @@
-from pymatgen.core import Structure
-from pymatgen.io import cif
-import numpy as np
+import sklearn.datasets as datasets
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.decomposition import PCA
+import pydotplus
+from six import StringIO
+from sklearn.tree import export_graphviz
 import matplotlib.pyplot as plt
+# Run this to add the path to the graphviz package
+import os
+
+os.environ['PATH'] = os.environ['PATH'] + ';' + r"C:\Program Files\Graphviz\bin"
 
 
 def main():
-    input_path = "S:\\projects\\12_LLTO_U2_N0_OV1\\LLTO_U2_N0_OV1_2\\STEP2"
-    structure_from_cif = Structure.from_file(input_path)
-    species = [s.specie.Z for s in structure_from_cif]
-    o_index_list = np.where(np.array(species) == 8)[0].tolist()
-    li_index_list = np.where(np.array(species) == 3)[0].tolist()
-    la_index_list = np.where(np.array(species) == 57)[0].tolist()
-    ti_index_list = np.where(np.array(species) == 22)[0].tolist()
+    data = pd.read_csv('energetics.csv')
+
+    # Print the first 5 rows of the dataframe
+    print(data.head())
+
+    # Prepare the data for the model
+    X = data.iloc[:, 1:-1].values
+    y = data.iloc[:, -1].values
+
+    # Split the data into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+    # Create the model
+    regressor = RandomForestRegressor()
+
+    pipe = Pipeline([('scaler', StandardScaler()), ('reduce_dim', PCA()),
+                     ('regressor', regressor)])
+    pipe.fit(X_train, y_train)
+    ypipe = pipe.predict(X_test)
+
+    dot_data = StringIO()
+    export_graphviz(pipe.named_steps['regressor'].estimators_[0],
+                    out_file=dot_data)
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    graph.write_png('tree.png')
+
+    importances = list(regressor.feature_importances_)
+    # List of tuples with variable and importance
+    print(importances)
+
+    # Saving feature names for later use
+    feature_list = list(data.columns)[1:-1]
+
+    feature_importances = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importances)]
+    # Sort the feature importances by most important first
+    feature_importances = sorted(feature_importances, key=lambda x: x[1], reverse=True)
+    # Print out the feature and importances
+    # [print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importances];
+
+    # Set the style
+    # plt.style.use('fivethirtyeight')
+    # list of x locations for plotting
+    x_values = list(range(len(importances)))
+    print(x_values)
+    # Make a bar chart
+    plt.bar(x_values, importances, orientation='vertical')
+    # Tick labels for x axis
+    plt.xticks(x_values, feature_list, rotation=6)
+    # Axis labels and title
+    plt.ylabel('Importance')
+    plt.xlabel('Variable')
+    plt.title('Variable Importances')
+    plt.show()
+
+
+if __name__ == '__main__':
+    main()
