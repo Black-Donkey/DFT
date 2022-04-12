42e6c369d606fbb00e43118276755929e17de139
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-04-12 17:00:10 -0400

    update v2

--- Machine_Learning/model_train.py
+++ Machine_Learning/model_train.py
@@ -1,73 +1,86 @@
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.pipeline import Pipeline
+from sklearn.metrics import mean_squared_error
+from sklearn.metrics import r2_score
 from sklearn.tree import export_graphviz
 from six import StringIO
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import pydotplus
 import os
 
 # Run this to add the path to the graphviz package
 os.environ['PATH'] = os.environ['PATH'] + ';' + r"C:\Program Files\Graphviz\bin"
 
+# Load the data
+data = pd.read_csv('energetics.csv')
 
-def main():
-    data = pd.read_csv('energetics.csv')
+# Print the first 5 rows of the dataframe
+print(data.head())
 
-    # Print the first 5 rows of the dataframe
-    print(data.head())
+# Prepare the data for the model
+X = data.iloc[:, 1:-1].values
+y = data.iloc[:, -1].values
 
-    # Prepare the data for the model
-    X = data.iloc[:, 1:-1].values
-    y = data.iloc[:, -1].values
+# Split the data into training and testing sets
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.1)
 
-    # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+# Create the model
+regressor = RandomForestRegressor()
 
-    # Create the model
-    regressor = RandomForestRegressor()
+pipe = Pipeline([('scaler', StandardScaler()), ('reduce_dim', PCA()), ('regressor', regressor)])
+pipe.fit(x_train, y_train)
+y_pred_RF = pipe.predict(x_test)
 
-    pipe = Pipeline([('scaler', StandardScaler()), ('reduce_dim', PCA()), ('regressor', regressor)])
-    pipe.fit(X_train, y_train)
-    ypipe = pipe.predict(X_test)
+dot_data = StringIO()
+export_graphviz(pipe.named_steps['regressor'].estimators_[0], out_file=dot_data)
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+graph.write_png('tree.png')
 
-    dot_data = StringIO()
-    export_graphviz(pipe.named_steps['regressor'].estimators_[0],
-                    out_file=dot_data)
-    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-    graph.write_png('tree.png')
+importance = list(regressor.feature_importances_)
+# List of tuples with variable and importance
 
-    importance = list(regressor.feature_importances_)
-    # List of tuples with variable and importance
-    print(importance)
+# Saving feature names for later use
+feature_list = list(data.columns)[1:-1]
 
-    # Saving feature names for later use
-    feature_list = list(data.columns)[1:-1]
+feature_importance = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importance)]
+# Sort the feature importance by most important first
+feature_importance = sorted(feature_importance, key=lambda x: x[1], reverse=True)
+# Print out the feature and importance
+[print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importance]
 
-    feature_importance = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importance)]
-    # Sort the feature importance by most important first
-    feature_importance = sorted(feature_importance, key=lambda x: x[1], reverse=True)
-    # Print out the feature and importance
-    [print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importance]
+# Set the style
+plt.style.use('bmh')
+# list of x locations for plotting
+x_values = list(range(len(importance)))
+# Make a bar chart
+plt.bar(x_values, importance, orientation='vertical')
+# Tick labels for x axis
+plt.xticks(x_values, feature_list, rotation=6)
+# Axis labels and title
+plt.ylabel('Importance')
+plt.xlabel('Variable')
+plt.title('Variable Importance')
+plt.show()
 
-    # Set the style
-    plt.style.use('bmh')
-    # list of x locations for plotting
-    x_values = list(range(len(importance)))
-    print(x_values)
-    # Make a bar chart
-    plt.bar(x_values, importance, orientation='vertical')
-    # Tick labels for x axis
-    plt.xticks(x_values, feature_list, rotation=6)
-    # Axis labels and title
-    plt.ylabel('Importance')
-    plt.xlabel('Variable')
-    plt.title('Variable Importance')
-    plt.show()
+# creating an object of LinearRegression class
+LR = LinearRegression()
+# fitting the training data
+LR.fit(x_train, y_train)
+y_pred_MLR = LR.predict(x_test)
 
+# predicting the accuracy score
+RF_r2score = r2_score(y_test, y_pred_RF)
+MLR_r2score = r2_score(y_test, y_pred_MLR)
+RF_mse = mean_squared_error(y_test, y_pred_RF)
+MLR_mse = mean_squared_error(y_test, y_pred_MLR)
 
-if __name__ == '__main__':
-    main()
+print("RF r2 score is ", RF_r2score)
+print("MLR r2 score is ", MLR_r2score)
+print("RF mse is ", RF_mse)
+print("MLR mse is ", MLR_mse)
