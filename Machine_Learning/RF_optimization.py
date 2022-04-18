92027b485ec45eeb411830b9b0aae0fbeb3e7c2d
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-04-18 12:13:57 -0400

    update plot

--- Machine_Learning/RF_optimization.py
+++ Machine_Learning/RF_optimization.py
@@ -1,15 +1,8 @@
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import RandomizedSearchCV
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
 from sklearn.ensemble import RandomForestRegressor
-from sklearn.pipeline import Pipeline
-from sklearn.tree import export_graphviz
-from six import StringIO
-import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
-import pydotplus
 import os
 
 # Run this to add the path to the graphviz package
@@ -56,44 +49,11 @@ print(random_grid)
 # Use the random grid to search for best hyperparameters
 # First create the base model to tune
 rf = RandomForestRegressor()
-# Random search of parameters, using 3 fold cross validation,
+
+# Random search of parameters, using 3-fold cross validation,
 # search across 100 different combinations, and use all available cores
 rf_random = RandomizedSearchCV(estimator=rf, param_distributions=random_grid, n_iter=100, cv=3, verbose=2,
                                random_state=42, n_jobs=-1)
 # Fit the random search model
 rf_random.fit(x_train, y_train)
 
-# pipe = Pipeline([('scaler', StandardScaler()), ('reduce_dim', PCA()), ('regressor', regressor)])
-# pipe.fit(x_train, y_train)
-# y_pred_RF = pipe.predict(x_test)
-#
-# dot_data = StringIO()
-# export_graphviz(pipe.named_steps['regressor'].estimators_[0], out_file=dot_data)
-# graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
-# graph.write_png('tree.png')
-#
-# importance = list(regressor.feature_importances_)
-# # List of tuples with variable and importance
-#
-# # Saving feature names for later use
-# feature_list = list(data.columns)[1:-1]
-#
-# feature_importance = [(feature, round(importance, 2)) for feature, importance in zip(feature_list, importance)]
-# # Sort the feature importance by most important first
-# feature_importance = sorted(feature_importance, key=lambda x: x[1], reverse=True)
-# # Print out the feature and importance
-# [print('Variable: {:20} Importance: {}'.format(*pair)) for pair in feature_importance]
-#
-# # Set the style
-# plt.style.use('bmh')
-# # list of x locations for plotting
-# x_values = list(range(len(importance)))
-# # Make a bar chart
-# plt.bar(x_values, importance, orientation='vertical')
-# # Tick labels for x axis
-# plt.xticks(x_values, feature_list, rotation=6)
-# # Axis labels and title
-# plt.ylabel('Importance')
-# plt.xlabel('Variable')
-# plt.title('Variable Importance')
-# plt.show()
