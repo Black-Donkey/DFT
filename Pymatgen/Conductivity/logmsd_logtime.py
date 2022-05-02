57039f0e1ca186554f5a2f4cc3fa1e8e55c6f819
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-05-02 02:54:16 -0400

    update plot

--- Pymatgen/Conductivity/logmsd_logtime.py
+++ Pymatgen/Conductivity/logmsd_logtime.py
@@ -8,7 +8,7 @@ from pymatgen.analysis.diffusion.aimd.pathway import ProbabilityDensityAnalysis
 # from pymatgen.analysis.diffusion.aimd.van_hove import VanHoveAnalysis
 
 
-path_U2_N0_OV0_1300K = ["S:/projects/25_LLTO_U2_MD/LLTO_U2_N0_OV0_1500K/run1/vasprun.xml",
+path_U2_N0_OV0_1500K = ["S:/projects/25_LLTO_U2_MD/LLTO_U2_N0_OV0_1500K/run1/vasprun.xml",
                         "S:/projects/25_LLTO_U2_MD/LLTO_U2_N0_OV0_1500K/run2/vasprun.xml",
                         "S:/projects/25_LLTO_U2_MD/LLTO_U2_N0_OV0_1500K/run3/vasprun.xml"]
 
@@ -35,3 +35,8 @@ plt.scatter(timelog, msdlog)
 plt.xlabel('Log(time)')
 plt.ylabel('Log(MSD)')
 plt.show()
+
+structure = analyzer_1500[1500].structure
+trajectories = [s.frac_coords for s in analyzer_1500[1500].get_drift_corrected_structures()]
+pda = ProbabilityDensityAnalysis(structure, trajectories, species="Li")
+pda.to_chgcar("CHGCAR.vasp")  # Output to a CHGCAR-like file for visualization in VESTA.
