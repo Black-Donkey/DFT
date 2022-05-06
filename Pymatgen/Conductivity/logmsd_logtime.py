330eba0a9faa474ddbe05c96bd2a83f4a53d9998
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-05-06 16:52:42 -0400

    update v2

--- Pymatgen/Conductivity/logmsd_logtime.py
+++ Pymatgen/Conductivity/logmsd_logtime.py
@@ -4,13 +4,17 @@ import numpy as np
 from pymatgen.core import Structure
 from pymatgen.analysis.diffusion.analyzer import DiffusionAnalyzer, get_arrhenius_plot, get_extrapolated_conductivity
 from pymatgen.analysis.diffusion.aimd.pathway import ProbabilityDensityAnalysis
+from pymatgen.core.trajectory import Trajectory
+from pymatgen.io.vasp.outputs import Xdatcar
+from pymatgen.analysis.diffusion_analyzer import DiffusionAnalyzer
 
 # from pymatgen.analysis.diffusion.aimd.van_hove import VanHoveAnalysis
 
 path_U1_N0_OV1_1500K = ["S:/projects/25_LLTO_U2_MD/LLTO_U1_N0_OV1_1500K/run1/vasprun.xml"]
 path_U1_N0_OV1_2000K = ["S:/projects/56_AIMD/LLTO_U1_N0_OV1_1/run1/vasprun.xml"]
 
-analyzer_2000 = DiffusionAnalyzer.from_files(path_U1_N0_OV1_1500K, specie="Li", smoothed=False)
+# analyzer_2000 = DiffusionAnalyzer.from_files(path_U1_N0_OV1_1500K, specie="Li", smoothed=False)
+analyzer_2000 = DiffusionAnalyzer.from_vaspruns(path_U1_N0_OV1_2000K, specie="Li")
 
 msd = analyzer_2000.msd[1:]
 time = analyzer_2000.dt[1:]
@@ -36,3 +40,8 @@ structure = analyzer_2000.structure
 trajectories = [s.frac_coords for s in analyzer_2000.get_drift_corrected_structures()]
 pda = ProbabilityDensityAnalysis(structure, trajectories, species=["Li"])
 pda.to_chgcar("CHGCAR.vasp")  # Output to a CHGCAR-like file for visualization in VESTA.
+
+traj = Trajectory.from_file('XDATCAR')
+diff = DiffusionAnalyzer.from_structures(traj, 'Li', 900, 2, 1)
+pda = ProbabilityDensityAnalysis.from_diffusion_analyzer(diff, interval=0.5, species=(["Li"]))
+pda.to_chgcar(filename="pda.vasp")
