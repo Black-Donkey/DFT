6145566dae2dd089871abc2d1519bf758885c48f
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-05-06 17:00:54 -0400

    update v2

--- Pymatgen/Conductivity/logmsd_logtime.py
+++ Pymatgen/Conductivity/logmsd_logtime.py
@@ -10,11 +10,11 @@ from pymatgen.analysis.diffusion_analyzer import DiffusionAnalyzer
 
 # from pymatgen.analysis.diffusion.aimd.van_hove import VanHoveAnalysis
 
-path_U1_N0_OV1_1500K = ["S:/projects/25_LLTO_U2_MD/LLTO_U1_N0_OV1_1500K/run1/vasprun.xml"]
-path_U1_N0_OV1_2000K = ["S:/projects/56_AIMD/LLTO_U1_N0_OV1_1/run1/vasprun.xml"]
+vasprun_U1_N0_OV1_2000K = ["S:/projects/56_AIMD/LLTO_U1_N0_OV1_1/run1/vasprun.xml"]
+xdatcar_U1_N0_OV1_2000K = "S:/projects/56_AIMD/LLTO_U1_N0_OV1_1/run1/XDATCAR"
 
 # analyzer_2000 = DiffusionAnalyzer.from_files(path_U1_N0_OV1_1500K, specie="Li", smoothed=False)
-analyzer_2000 = DiffusionAnalyzer.from_vaspruns(path_U1_N0_OV1_2000K, specie="Li")
+analyzer_2000 = DiffusionAnalyzer.from_files(vasprun_U1_N0_OV1_2000K, specie="Li", smoothed=False)
 
 msd = analyzer_2000.msd[1:]
 time = analyzer_2000.dt[1:]
@@ -36,12 +36,12 @@ plt.xlabel('Log(time)')
 plt.ylabel('Log(MSD)')
 plt.show()
 
-structure = analyzer_2000.structure
-trajectories = [s.frac_coords for s in analyzer_2000.get_drift_corrected_structures()]
-pda = ProbabilityDensityAnalysis(structure, trajectories, species=["Li"])
-pda.to_chgcar("CHGCAR.vasp")  # Output to a CHGCAR-like file for visualization in VESTA.
+# structure = analyzer_2000.structure
+# trajectories = [s.frac_coords for s in analyzer_2000.get_drift_corrected_structures()]
+# pda = ProbabilityDensityAnalysis(structure, trajectories, species=["Li"])
+# pda.to_chgcar("CHGCAR.vasp")  # Output to a CHGCAR-like file for visualization in VESTA.
 
-traj = Trajectory.from_file('XDATCAR')
-diff = DiffusionAnalyzer.from_structures(traj, 'Li', 900, 2, 1)
+traj = Trajectory.from_file(xdatcar_U1_N0_OV1_2000K)
+diff = DiffusionAnalyzer.from_structures(traj, 'Li', 2000, time_step=500, step_skip=1)
 pda = ProbabilityDensityAnalysis.from_diffusion_analyzer(diff, interval=0.5, species=(["Li"]))
 pda.to_chgcar(filename="pda.vasp")
