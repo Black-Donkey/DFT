910d619bd1c7e76673d8e9352eb1579a32d065ae
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-05-09 03:42:02 -0400

    update plot

--- Plot_Template/Energy_Plot.py
+++ Plot_Template/Energy_Plot.py
@@ -4,20 +4,19 @@ from matplotlib.ticker import MaxNLocator
 import pandas as pd
 
 
-path1 = os.path.dirname(os.getcwd()) + "\\Machine_Learning\\LLTO_Fingerprint\\energetics.csv"
-csv_energy = pd.read_csv(path1, header=None)
-# list_energy = sum(csv_energy.values.tolist(), [])
-#
-# La = [1, 2, 3, 4, 0, 1, 2, 3, 0, 1]
-# Li = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2]
-# plot_symbol = ["o", "^", "s"]
-#
-# for index in range(0, len(set(Li))):
-#     plot_La = [La[i] for i, x in enumerate(Li) if x == index]
-#     plot_energy = [list_energy[i] for i, x in enumerate(Li) if x == index]
-#     plt.scatter(plot_La, plot_energy, marker=plot_symbol[index], s=75)
-# plt.legend(['# Li=0', '# Li=1', '# Li=2'])
-# plt.xlabel('# La')
-# plt.ylabel('Energy (eV)')
-# plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
-# plt.show()
+path1 = os.path.dirname(os.getcwd()) + '\\Machine_Learning\\energetics.csv'
+csv_energy = pd.read_csv(path1)
+list_energy = csv_energy['E'].values.tolist()
+N = csv_energy['N'].values.tolist()
+OV = csv_energy['OV'].values.tolist()
+plot_symbol = ["o", "^", "s"]
+
+for index in range(0, len(set(OV))):
+    plot_N = [N[i] for i, x in enumerate(OV) if x == index]
+    plot_energy = [list_energy[i] for i, x in enumerate(OV) if x == index]
+    plt.scatter(plot_N, plot_energy, marker=plot_symbol[index], s=50)
+plt.legend(['# OV=0', '# OV=1', '# OV=2'])
+plt.xlabel('# N dopant')
+plt.ylabel('Energy (eV)')
+plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.show()
