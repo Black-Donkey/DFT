81d7f7079df65f165000f51970e2b6503bb74335
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-05-10 17:36:27 -0400

    update v2

--- Pymatgen/Conductivity/logmsd_logtime.py
+++ Pymatgen/Conductivity/logmsd_logtime.py
@@ -11,7 +11,6 @@ from pymatgen.io.vasp.outputs import Xdatcar
 
 vasprun_U1_N0_OV1_2000K = ["S:/projects/56_AIMD/LLTO_U1_N0_OV1_1/run1/vasprun.xml",
                            "S:/projects/56_AIMD/LLTO_U1_N0_OV1_1/run2/vasprun.xml"]
-xdatcar_U1_N0_OV1_2000K = "S:/projects/56_AIMD/LLTO_U1_N0_OV1_1/run1/XDATCAR"
 
 # analyzer_2000 = DiffusionAnalyzer.from_files(path_U1_N0_OV1_1500K, specie="Li", smoothed=False)
 analyzer_2000 = DiffusionAnalyzer.from_files(vasprun_U1_N0_OV1_2000K, specie="Li", smoothed=False)
@@ -41,7 +40,3 @@ trajectories = [s.frac_coords for s in analyzer_2000.get_drift_corrected_structu
 pda = ProbabilityDensityAnalysis(structure, trajectories, species=["Li"])
 pda.to_chgcar("CHGCAR.vasp")  # Output to a CHGCAR-like file for visualization in VESTA.
 
-# traj = Trajectory.from_file(xdatcar_U1_N0_OV1_2000K)
-# diff = DiffusionAnalyzer.from_structures(traj, 'Li', 2000, time_step=500, step_skip=1)
-# pda = ProbabilityDensityAnalysis.from_diffusion_analyzer(diff, interval=0.5, species=(["Li"]))
-# pda.to_chgcar(filename="pda.vasp")
