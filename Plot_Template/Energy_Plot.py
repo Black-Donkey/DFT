ec33b5db545b1dd33c2489f72bbf6c3f1b173a71
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-05-12 03:11:41 -0400

    update plot

--- Plot_Template/Energy_Plot.py
+++ Plot_Template/Energy_Plot.py
@@ -9,14 +9,16 @@ csv_energy = pd.read_csv(path1)
 list_energy = csv_energy['E'].values.tolist()
 N = csv_energy['N'].values.tolist()
 OV = csv_energy['OV'].values.tolist()
-plot_symbol = ["o", "^", "s"]
+plot_symbol = ["o", "^"]
 
+plt.style.use('seaborn-darkgrid')
 for index in range(0, len(set(OV))):
     plot_N = [N[i] for i, x in enumerate(OV) if x == index]
     plot_energy = [list_energy[i] for i, x in enumerate(OV) if x == index]
-    plt.scatter(plot_N, plot_energy, marker=plot_symbol[index], s=50)
-plt.legend(['# OV=0', '# OV=1', '# OV=2'])
+    plt.plot(plot_N, plot_energy, plot_symbol[index], markersize=10, markerfacecolor='none')
+plt.legend(['# OV = 0', '# OV = 1'])
 plt.xlabel('# N dopant')
 plt.ylabel('Energy (eV)')
 plt.gca().xaxis.set_major_locator(MaxNLocator(integer=True))
+plt.savefig("energy_plot.svg", dpi=600, format="svg")
 plt.show()
